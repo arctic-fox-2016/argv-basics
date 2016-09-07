a3ddf7a13ac2cf3b8ad806669242a2433789a666
Kun Anggiar Lanang & Sahbana <pair+kun7.sahbanagold@hacktiv8.com>
2016-09-07 10:27:13 +0700

    simpel

--- pig_latin.js
+++ pig_latin.js
@@ -3,7 +3,7 @@
 let pigLatin = (word) => {
   let firstLetter = word.charAt(0);
   if(isPowel(firstLetter)){
-    return word
+    return word+"ay"
   }else{
     return `${word.substr(1)}${firstLetter}ay`;
   }
@@ -24,3 +24,15 @@ let convert = (sentence) => {
 }
 
 // Your CLI code here
+let argc = process.argv.length
+if(argc< 3){
+  console.log("Silahkan tambahkan argumen")
+    console.log("contoh : node pig_latin.js sahbana dan kun")
+} else{
+let wordy =""
+process.argv.forEach((val, index, array) => {
+
+   if(index > 1) wordy += pigLatin(val) + " "
+  })
+console.log(`${wordy}`)
+}
